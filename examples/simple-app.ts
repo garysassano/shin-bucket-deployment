1819e6eac807b39366ea1e402136646fe7712b76
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2026-03-29 20:38:51 +0200

    update biome config

--- examples/simple-app.ts
+++ examples/simple-app.ts
@@ -1,51 +1,40 @@
-import * as path from 'node:path';
-import {
-	App,
-	CfnOutput,
-	Fn,
-	RemovalPolicy,
-	Stack,
-	type StackProps,
-} from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import { CargoBucketDeployment, Source } from '../src';
+import * as path from "node:path";
+import { App, CfnOutput, Fn, RemovalPolicy, Stack, type StackProps } from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { CargoBucketDeployment, Source } from "../src";
 
 class SimpleCargoBucketDeploymentStack extends Stack {
-	constructor(scope: App, id: string, props?: StackProps) {
-		super(scope, id, props);
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-		const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-			autoDeleteObjects: true,
-			removalPolicy: RemovalPolicy.DESTROY,
-		});
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
-		const deployment = new CargoBucketDeployment(this, 'DeployWebsite', {
-			sources: [
-				Source.asset(
-					path.join(__dirname, '..', '..', 'test', 'fixtures', 'my-website'),
-				),
-			],
-			destinationBucket: websiteBucket,
-			destinationKeyPrefix: 'site',
-		});
+    const deployment = new CargoBucketDeployment(this, "DeployWebsite", {
+      sources: [Source.asset(path.join(__dirname, "..", "..", "test", "fixtures", "my-website"))],
+      destinationBucket: websiteBucket,
+      destinationKeyPrefix: "site",
+    });
 
-		new CfnOutput(this, 'BucketName', {
-			value: websiteBucket.bucketName,
-		});
+    new CfnOutput(this, "BucketName", {
+      value: websiteBucket.bucketName,
+    });
 
-		new CfnOutput(this, 'ObjectKeys', {
-			value: Fn.join(',', deployment.objectKeys),
-		});
-	}
+    new CfnOutput(this, "ObjectKeys", {
+      value: Fn.join(",", deployment.objectKeys),
+    });
+  }
 }
 
 const app = new App();
 const env =
-	process.env.CDK_DEFAULT_ACCOUNT && process.env.CDK_DEFAULT_REGION
-		? {
-				account: process.env.CDK_DEFAULT_ACCOUNT,
-				region: process.env.CDK_DEFAULT_REGION,
-			}
-		: undefined;
+  process.env.CDK_DEFAULT_ACCOUNT && process.env.CDK_DEFAULT_REGION
+    ? {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+      }
+    : undefined;
 
-new SimpleCargoBucketDeploymentStack(app, 'CargoBucketDeploymentDemo', { env });
+new SimpleCargoBucketDeploymentStack(app, "CargoBucketDeploymentDemo", { env });
--- test/token-replacement.test.ts
+++ test/token-replacement.test.ts
@@ -1,38 +1,38 @@
-import * as path from 'node:path';
-import * as cdk from 'aws-cdk-lib';
-import { Match, Template } from 'aws-cdk-lib/assertions';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import { test } from 'vitest';
-import { CargoBucketDeployment, Source } from '../src';
+import * as path from "node:path";
+import * as cdk from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { test } from "vitest";
+import { CargoBucketDeployment, Source } from "../src";
 
-test('renders source markers for jsonData sources', () => {
-	const stack = new cdk.Stack();
-	const destinationBucket = new s3.Bucket(stack, 'Dest');
+test("renders source markers for jsonData sources", () => {
+  const stack = new cdk.Stack();
+  const destinationBucket = new s3.Bucket(stack, "Dest");
 
-	new CargoBucketDeployment(stack, 'Deploy', {
-		sources: [
-			Source.asset(path.join(__dirname, 'fixtures', 'my-website')),
-			Source.jsonData(
-				'runtime/config.json',
-				{
-					stackName: cdk.Aws.STACK_NAME,
-					region: cdk.Aws.REGION,
-				},
-				{ escape: true },
-			),
-		],
-		destinationBucket,
-	});
+  new CargoBucketDeployment(stack, "Deploy", {
+    sources: [
+      Source.asset(path.join(__dirname, "fixtures", "my-website")),
+      Source.jsonData(
+        "runtime/config.json",
+        {
+          stackName: cdk.Aws.STACK_NAME,
+          region: cdk.Aws.REGION,
+        },
+        { escape: true },
+      ),
+    ],
+    destinationBucket,
+  });
 
-	const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-	template.hasResourceProperties('Custom::CargoBucketDeployment', {
-		SourceMarkers: Match.anyValue(),
-		SourceMarkersConfig: Match.arrayWith([
-			Match.objectLike({}),
-			Match.objectLike({ jsonEscape: true }),
-		]),
-		Extract: true,
-		Prune: true,
-	});
+  template.hasResourceProperties("Custom::CargoBucketDeployment", {
+    SourceMarkers: Match.anyValue(),
+    SourceMarkersConfig: Match.arrayWith([
+      Match.objectLike({}),
+      Match.objectLike({ jsonEscape: true }),
+    ]),
+    Extract: true,
+    Prune: true,
+  });
 });
